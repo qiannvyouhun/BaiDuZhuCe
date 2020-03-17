e3ad5569b84d722369828867fa874b1afe5c6cf1
qiannvyouhun <2504189164@qq.com>
2020-03-17 17:42:54 +0800

    .

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
             },
             files: {
                 src: './index.html',
-                dest: './compression/index.min.html'
+                dest: './compression/index.html'
             }
         },
         cssmin: {
